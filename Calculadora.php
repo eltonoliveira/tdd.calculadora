fbef92515705d46dd40261b66c2d189ba24eba49
Elton D. de Oliveira <elton-oliv@bol.com.br>
2013-01-07 00:56:58 -0200

    Calculadora: - Soma de dois números int negativos - Refatoração do classe de test para que o objeto  fosse instanciado no método setUP()

--- Calculadora.php
+++ Calculadora.php
@@ -4,10 +4,11 @@ class Calculadora
 {
    /**
    * @tutorial O método somar precisará garantir as seguintes somas:
-   * - Soma de dois números inteiros positivos - OK
-   * - Soma de dois números inteiros positivos e/ou negativos
-   * - Soma de dois números reais (float) positivos com duas casas decimais
-   * - Soma de dois números reais (float) positivos e/ou negativos com duas casas decimais
+   * - Soma de dois números int positivos - OK
+   * - Soma de dois números int negativos - OK
+   * - Soma de dois números float positivos com duas casas decimais
+   * - Soma de dois números float negativos com duas casas decimais
+   * - Soma de duas strings
    *
    * @name somar
    * @param int|float|string $valor1
--- testes/CalculadoraTest.php
+++ testes/CalculadoraTest.php
@@ -5,17 +5,30 @@ require_once '/usr/share/php/PHPUnit/Framework/TestCase.php';
 /** Nome da classe que será testada */
 require_once 'Calculadora.php';
 
+
 class CalculadoraTest extends PHPUnit_Framework_TestCase
 {
+    /**
+    * Objetos
+    * @var object
+    */
+    protected $_calculadora; 
+    
+    
+    /**
+    * Construtor da classe 
+    */
+    protected function setUp()
+    {
+        $this->_calculadora = new Calculadora();
+    }
+    
     /**
     * Testa se dois mais dois é igual a quatro
     */
     public function testSomarDoisMaisDois()
     {
-        $calculadora = new Calculadora();
-        
-        self::assertEquals(4, $calculadora->somar(2, 2));
-    
+        self::assertEquals(4, $this->_calculadora->somar(2, 2));
     }
     
     /**
@@ -23,9 +36,16 @@ class CalculadoraTest extends PHPUnit_Framework_TestCase
     */
     public function testSomarTresMaisTres()
     {
-        $calculadora = new Calculadora();
-        
-        self::assertEquals(6, $calculadora->somar(3 , 3));
+        self::assertEquals(6, $this->_calculadora->somar(3 , 3));
     }
+    
+    /**
+    * Testa se -5 mais -9 é igual a -14
+    */
+    public function testSomarDoisInteirosNegativos()
+    {
+        self::assertEquals(-14, $this->_calculadora->somar(-5, -9));
+    }
+    
     
 }
